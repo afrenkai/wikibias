968b3a8386f327ab0e1bd63600f9e9183a48a4ac
daniel <38931252+dgorbunov@users.noreply.github.com>
2024-11-03 15:14:54 -0500

    wikipedia api fetching

---
+++ fetch.py
@@ -0,0 +1,14 @@
+import wikipediaapi as wp
+
+def fetch_article(title):
+    wiki = wp.Wikipedia(
+        user_agent='WikiBias (sundai@mit.edu)',
+        language='en',
+        extract_format=wp.ExtractFormat.WIKI
+    )
+
+    page = wiki.page(title)
+    print(page.text)
+
+if __name__ == "__main__":
+    fetch_article("Python (programming language)")
