99dd65172cfa2e64d524119ba0af5fede8dca174
daniel <38931252+dgorbunov@users.noreply.github.com>
2024-11-03 19:50:02 -0500

    update consts

--- consts.py
+++ consts.py
@@ -1,15 +1,23 @@
 class Endpoints:
-    API = "/api"
     sentiment = "/sentiment"
     bias = "/bias"
     
 class Keys:
     title = "title"
-    sentiment = "sentiment"
-    bias = "bias"
+
+class JSON:
+    sentiment_key = "sentiment"
+    
+    bias_key = "bias"
+    side_key = "side"
+    
 
 class Sentiment:
     pos_threshold = 0.4
     neg_threshold = -0.4
     sentiment_key = "compound"
+
+class Bias:
+    left_threshold = 0.4
+    right_threshold = 0.4
     
